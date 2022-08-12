35071888683a32fbffc2b1129bc32fda01450d85
Anwar Barakat <brkatanwar0@gmail.com>
2022-08-12 16:32:32 +0300

    add nationalities table and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             GradeSeeder::class,
             ClassroomSeeder::class,
             BloodSeeder::class,
+            NationalitySeeder::class,
         ]);
     }
 }
